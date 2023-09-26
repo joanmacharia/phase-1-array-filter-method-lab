80155db59774b742f00381e18ab1a10780581d63
Joan Macharia <joan.macharia@students.moringa>
2023-09-26 11:29:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+ 
+function findMatching(drivers,driverName){
+    return drivers.filter((driver) => driver.toLowerCase() === driverName.toLowerCase() )
+}
+
+function fuzzyMatch(drivers,letters){
+    return drivers.filter((driver) => driver.startsWith(letters))
+}
+
+function matchName(drivers,driverName){
+    return drivers.filter((driver) => driver.name === driverName)
+}
